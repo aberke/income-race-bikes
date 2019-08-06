e8dae36a06af8d23b3bbd82620ac5801de932d14
Steve Gattuso <steve@stevegattuso.me>
2019-08-06 15:34:14 -0400

    an even better stopping point

--- viz/median-income.js
+++ viz/median-income.js
@@ -1,5 +1,16 @@
 const MAPBOX_URL = 'https://api.mapbox.com/styles/v1/steifineo/cjyuf2hgv01so1cpe8u9yjw32/tiles/256/{z}/{x}/{y}?access_token={accessToken}';
 
+let layer;
+const selectYear = (year) => {
+  if (layer) {
+    layer.remove();
+  }
+
+  layer = L
+    .geoJson(nycJson, {style: style(year)})
+    .addTo(map);
+};
+
 const map = L
   .map('map', {preferCanvas: true})
   .setView([40.691425, -73.987242], 12);
@@ -13,12 +24,18 @@ L.tileLayer(MAPBOX_URL, {
 
 const colorRange = d3
   .scaleLinear()
-  .domain([10000, 120000])
+  .domain([20000, 140000])
   .range(['rgba(0, 255, 0, 0)', 'rgba(0, 255, 0, 1)']);
 
 let i = 0;
-const style = (feature) => {
-  const income = feature.properties['2017 median income']
+const style = (year) => (feature) => {
+  let income = feature.properties[`${year} median income`];
+  if (income === '250,000+') {
+    income = 250000;
+  } else if (isNaN(parseInt(income))) {
+    console.log(feature.properties['Neighborhood'], income);
+    income = 0;
+  }
 
   return {
     fillColor: colorRange(income),
@@ -26,6 +43,4 @@ const style = (feature) => {
   };
 };
 
-L
-  .geoJson(nycJson, {style})
-  .addTo(map);
+selectYear('2017');
