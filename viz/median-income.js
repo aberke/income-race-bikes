d991a1459910dd724bde4c87be2f3a0c2738e167
aberke <berke.alexandra@gmail.com>
2019-08-14 10:30:46 -0400

    renamed stations to nyc-stations

--- viz/median-income.js
+++ viz/median-income.js
@@ -317,7 +317,6 @@ raceCheck.addEventListener('click', toggleRaceLayer);
 // load the data
 let xhr = new XMLHttpRequest();
 xhr.open('GET', './viz/nyc-census-tracts-data.geojson');
-// xhr.open('GET', './data/ny/nyc-census-tracts-data.geojson');
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.responseType = 'json';
 xhr.onload = function() {
